bfbbcfd5f11634edd766dc18827cd70b3bf273f1
GabrielMendonca1 <garimeipod@gmail.com>
2026-09-01 10:13:59 -0300

    chore: title G

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Gabriel Mendonça - About Me",
+  title: "G",
   description:
     "A personal timeline of Gabriel Mendonça's work, projects, and ideas.",
 };
